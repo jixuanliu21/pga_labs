2d57c734771f57a9736fb4b576f629810fab82e3
jixuanliu21 <jixuanliu21@outlook.com>
2026-06-28 20:09:06 +0800

    Lab 4: Intersection of sets and selection sort algorithm

---
+++ selection.c
@@ -0,0 +1,42 @@
+//Selection Sort
+#include <stdio.h>
+
+// Function to help print the array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// Selection sort function
+void selectionSort(int arr[], int n) {
+    int i, j, min_idx;
+
+    for (i = 0; i < n - 1; i++) {
+        min_idx = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        // swap the smallest with the current
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Before: \n");
+    printArray(arr, n);
+
+    selectionSort(arr, n);
+
+    printf("After: \n");
+    printArray(arr, n);
+
+    return 0;
+}
