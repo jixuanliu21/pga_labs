b7359f79fffc6adec675fb5f81889865784c470e
jixuanliu21 <jixuanliu21@outlook.com>
2026-07-01 19:33:53 +0800

    Fix nested repo and restore labs

--- lab3/sort.c
+++ lab3/sort.c
@@ -1,33 +1,33 @@
-//Bubble sort
-#include <iostream>
-using namespace std;
-
-int a[102];
-void bubble_sort(int n){
-    int i,j;
-    for (i=0;i<n-1;i++){
-        bool swapped=false;
-        for (j=0;j<n-i-1;j++){
-            if (a[j]>a[j+1]){
-                swap(a[j],a[j+1]);
-                swapped=true;
-            }
-        }
-        if (!swapped) break;
-    }
-}
-
+//Exercise:Sorting an array of numbers
+#include<stdio.h>
 int main(){
-    int n,i;
+    int n,i,j,temp;
+    printf("Enter number of mark:");
     scanf("%d",&n);
 
+    int arr[n];
+
+    //Get input number from user
     for (i=0;i<n;i++){
-        scanf("%d",&a[i]);
-        bubble_sort(n);
-    }
-    for (i=0;i<n;i=+){
-        printf("%d ",a[i]);
+        printf("Enter mark %d: ",i+1);
+        scanf("%d",&arr[i]);
     }
 
+    //Bubble sort (Descending order)
+    //Outer loop: Controls (n-1) loops
+    for (i=0;i<n-1;i++){
+        //Inner loop: Compare adjacent elements each time
+        for (j=0;j<n-1-i;j++){
+            //If the former element is smaller, swap
+             if(arr[j] < arr[j+1]) {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    for(i = 0; i < n; i++) {
+        printf("%d\n", arr[i]);
+    }
     return 0;
 }
